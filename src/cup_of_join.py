c268f75b16eeb013e03669f784bf4c615b196cca
Meron <meroncabag110@gmail.com>
2025-03-31 23:02:14 +0300

    added cup_of_join

---
+++ src/cup_of_join.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 26 22:33:59 2025
+
+@author: meron
+"""
+
+def cup_of_join(*lists, sep = "-"):
+    ret = []
+    for lst in lists:
+        ret.extend(lst)
+        ret.append(sep)
+    ret.pop()
+    return ret
+print(join([1,2],[5],[1,3,6],sep = '|'))
