1461aaae915fa80a83ada8126f5ea2bd06886da8
Meron <meroncabag110@gmail.com>
2025-03-31 22:53:05 +0300

    added communication vessel

---
+++ src/communicating_vessels.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 27 01:37:06 2025
+
+@author: meron
+"""
+
+def interleave(*itera):
+    ret = []
+    bigest = 0
+    for ite in itera:
+        if len(ite)>bigest:
+            bigest = len(ite)
+    for i in range(bigest):
+        for ite in itera:
+            if i < len(ite):
+                ret.append(ite[i])
+    return ret
+
+print(interleave('abc', [1, 2, 3], ('!', '@', '#')))
+#['a', 1, '!', 'b', 2, '@', 'c', 3, '#']       
