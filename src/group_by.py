d992aebb04f6655e25edad3560303ba1322bf30f
Meron <meroncabag110@gmail.com>
2025-03-31 23:03:39 +0300

    added group by

---
+++ src/group_by.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar 30 14:08:59 2025
+
+@author: meron
+"""
+
+def group_by(func,lst):
+    ret = {}
+    for i in lst:
+        ret[func(i)] = []
+    for i in lst:
+        ret[func(i)].append(i)
+    return ret
+    
+
+
+print(group_by(len, ["hi", "bye", "yo", "try"]))
