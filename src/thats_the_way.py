f4e420d0ca2293298a048173737d8d5b4818a4ed
Meron <meroncabag110@gmail.com>
2025-04-01 12:06:19 +0300

    update file

---
+++ src/thats_the_way.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 26 22:04:46 2025
+
+@author: meron
+"""
+import os
+def thats_the_way(directory):
+    files = os.listdir(directory)
+    deep_files = [file for file in files if file.startswith("deep")]
+    return deep_files
+ 
